0d99bf449ce91c52ec04dab4b325568a07b437fb
Steven Bull <stevenwbulljr@gmail.com>
2019-10-15 12:43:58 +0000

    finish authentication service and router, initial setup jwt-auth middleware

--- auth/auth-service.js
+++ auth/auth-service.js
@@ -3,7 +3,23 @@ const jwt = require('jsonwebtoken');
 const config = require('../src/config');
 
 const AuthService = {
-
+  getUserWithUserName(db, user_name) {
+    return db('rr_users')
+      .where({ user_name })
+      .first();
+  },
+  comparePassword(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      algorithm: 'HS256'
+    });
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET,{ algorithms: ['HS256']});
+  }
 };
 
 module.exports = AuthService;

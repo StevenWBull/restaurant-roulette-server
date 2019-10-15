e58f4429fb026b53a95bcd1fec135534acf40b10
Steven Bull <stevenwbulljr@gmail.com>
2019-10-15 12:19:29 +0000

    add authentication folder and files, begin logic

---
+++ auth/auth-router.js
@@ -0,0 +1,3 @@
+const express = require('express');
+
+const AuthService = require('./auth-service');
---
+++ auth/auth-service.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypyjs');
+const jwt = require('jsonwebtoken');
+const config = require('../src/config');
+
+const AuthService = {
+
+};
+
+module.exports = AuthService;

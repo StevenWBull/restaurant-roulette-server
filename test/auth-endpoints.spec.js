c480f55a7753c2bb60f48fc832d37295e0adb4b9
Steven Bull <stevenwbulljr@gmail.com>
2019-10-15 16:47:43 +0000

    add initial mocha/chai hooks to auth-endpoints.spec.js

---
+++ test/auth-endpoints.spec.js
@@ -0,0 +1,22 @@
+const knex = require('knex');
+const app = require('../src/app');
+const jwt = require('jsonwebtoken');
+const helpers = require('./test-helpers');
+
+describe.only('Auth Endpoints', () => {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('cleanup', () => helpers.cleanTables(db));
+
+  afterEach('cleanup', () => helpers.cleanTables(db));
+});
